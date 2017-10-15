a4ed1a38e052cbda73296fbbdb2f8905cc678db1
Taehwan Kim <maxtortime@gmail.com>
2017-10-15 23:35:39 +0900

    boj 10819 10971 1697 6603

---
+++ BOJ/1697.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, K;
+    queue<int> q;
+    int s = 1000000;
+
+    cin >> N >> K;
+    q.push(N);
+
+    while 
+
+    
+    return 0;
+}
+
+
+
